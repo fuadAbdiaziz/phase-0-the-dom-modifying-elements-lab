0aaf0d5673f640bb83c188a0ccee025041a14b11
Fuad <fuadabdaz99@gmail.com>
2023-03-25 11:55:12 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// Get a reference to the main element
+const main = document.getElementById('main');
+
+// Remove the main element from the DOM
+main.remove();
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Replace "YOUR-NAME" with your name
+newHeader.textContent = newHeader.textContent.replace('YOUR-NAME', 'John Doe');
+
+// Append the new h1 element to the body
+document.body.appendChild(newHeader);
